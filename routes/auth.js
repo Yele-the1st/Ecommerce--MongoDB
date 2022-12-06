7aa2870d722f288e2024ee6af8cce1dfd122ec5a
Omoyele Olabode <95770630+Yele-the1st@users.noreply.github.com>
2022-12-06 12:14:23 +0100

    Add files via upload

---
+++ routes/auth.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const { check, body } = require("express-validator");
+
+const authController = require("../controllers/auth");
+const User = require("../models/user");
+
+const router = express.Router();
+
+router.get("/login", authController.getLogin);
+
+router.get("/signup", authController.getSignup);
+
+router.post(
+  "/login",
+  [
+    body("email")
+      .isEmail()
+      .withMessage("please enter a valid email address.")
+      .normalizeEmail({ gmail_remove_dots: false }),
+    body("password", "Password has to be valid.")
+      .isLength({ min: 5 })
+      .isAlphanumeric()
+      .trim(),
+  ],
+  authController.postLogin
+);
+
+router.post(
+  "/signup",
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Please enter a valid email")
+      .custom((value, { req }) => {
+        // if (value === "test1@test.com") {
+        //   throw new Error("This email address is forbidden");
+        // }
+        // return true;
+        return User.findOne({ email: value }).then((userDoc) => {
+          if (userDoc) {
+            return Promise.reject(
+              "Email exists already, please pick a different one."
+            );
+          }
+        });
+      })
+      .normalizeEmail({ gmail_remove_dots: false }),
+    body(
+      "password",
+      "please enter a password with only numbers and text and at least 5 characters"
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric()
+      .trim(),
+    body("confirmPassword")
+      .trim()
+      .custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error("Passwords have to match!");
+        }
+        return true;
+      }),
+  ],
+  authController.postSignup
+);
+
+router.post("/logout", authController.postLogout);
+
+router.get("/reset", authController.getReset);
+
+router.post(
+  "/reset",
+  [
+    body("email")
+      .isEmail()
+      .withMessage("please enter a valid email address.")
+      .normalizeEmail({ gmail_remove_dots: false }),
+  ],
+  authController.postReset
+);
+
+router.get("/reset/:token", authController.getNewPassword);
+
+router.post("/new-password", authController.postNewPassword);
+
+module.exports = router;
